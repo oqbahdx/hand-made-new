3cbf9cd6895f04dfb5a64f4668b2fbfcf889378b
oqbah dx <oqbahdx@gmail.com>
2021-10-12 21:53:54 +0200

    fix start page

--- lib/screens/home/start.dart
+++ lib/screens/home/start.dart
@@ -4,6 +4,7 @@ import 'package:hand_made_new/screens/home/profile.dart';
 import 'package:hand_made_new/screens/home/time_line.dart';
 import 'package:hand_made_new/state_management/cubit.dart';
 import 'package:hand_made_new/state_management/states.dart';
+import 'package:hand_made_new/styles/fonts.dart';
 import 'package:hand_made_new/widgets/app_bar.dart';
 import 'package:hand_made_new/widgets/drawer_widget.dart';
 import 'package:hand_made_new/widgets/start_widget.dart';
@@ -20,59 +21,64 @@ class StartPage extends StatefulWidget {
 
 class _StartPageState extends State<StartPage> {
   int currentIndex = 1;
-  List<Widget> homePages = [FamiliesList(), MapPage(), TimeLine(), Profile()];
-  List<String> titles = ['Families List','Map','TimeLine','Account'];
+  List<Widget> homePages = [Profile(),MapPage(),TimeLine(), FamiliesList(), ];
+  List<String> titles = [
+    'Profile',
+    'Map',
+    'TimeLine',
+    'Families List',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context)=>HandCubit(),
-      child: BlocConsumer<HandCubit,HandMadeState>(
-        listener: (context,state){},
-        builder: (context,state){
-         return Directionality(
-           textDirection: TextDirection.rtl,
-           child: Scaffold(
-             appBar: appBarWidget(
-               title: Text(titles[HandCubit.get(context).pageIndex],style: TextStyle(
-                 fontWeight: FontWeight.bold,
-                 fontFamily: 'Amiri'
-               ),),
-               elevation: 0.0,
-               action: Container()
-             ),
-               body: Center(
-                 child: homePages.elementAt(HandCubit.get(context).pageIndex),
-               ),
-               bottomNavigationBar: Container(
-                   decoration: BoxDecoration(
-                     color: Colors.white,
-                     boxShadow: [
-                       BoxShadow(
-                         blurRadius: 20,
-                         color: Colors.black.withOpacity(.1),
-                       )
-                     ],
-                   ),
-                   child: SafeArea(
-                     child: Padding(
-                         padding:
-                         const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-                         child: startBottomNav(
-                             index: HandCubit.get(context).pageIndex,
-                             function: (index){
-                               HandCubit.get(context).changeIndex(index);
-                             },
-                               gButton: [
-                               gButtonNav(icon: Icons.home, text: 'home'),
-                               gButtonNav(icon: Icons.map, text: 'map'),
-                               gButtonNav(
-                                   icon: Icons.ten_k_outlined, text: 'time line'),
-                               gButtonNav(icon: Icons.person, text: 'profile'),
-                             ])),
-                   )),drawer: drawerBuild(context),),
-         );
+      create: (context) => HandCubit(),
+      child: BlocConsumer<HandCubit, HandMadeState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Directionality(
+            textDirection: TextDirection.rtl,
+            child: Scaffold(
+              appBar: appBarWidget(
+                  title: Text(titles[HandCubit.get(context).pageIndex],
+                      style: normalText),
+                  elevation: 0.0,
+                  action: Container()),
+              body: Center(
+                child: homePages.elementAt(HandCubit.get(context).pageIndex),
+              ),
+              bottomNavigationBar: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 20,
+                        color: Colors.black.withOpacity(.1),
+                      )
+                    ],
+                  ),
+                  child: SafeArea(
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15.0, vertical: 8),
+                        child: startBottomNav(
+                            index: HandCubit.get(context).pageIndex,
+                            function: (index) {
+                              HandCubit.get(context).changeIndex(index);
+                            },
+                            gButton: [
+                              gButtonNav(icon: Icons.person, text: 'profile'),
+                              gButtonNav(icon: Icons.map, text: 'map'),
+                              gButtonNav(
+                                  icon: Icons.ten_k_outlined,
+                                  text: 'time line'),
+                              gButtonNav(icon: Icons.home, text: 'home'),
+                            ])),
+                  )),
+              drawer: drawerBuild(context),
+            ),
+          );
         },
-
       ),
     );
   }
