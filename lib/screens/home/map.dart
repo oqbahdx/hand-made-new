9de05d7e5baabf96b072c3730239751ec5bb4305
oqbahdx <oqbahdx@gmail.com>
2022-01-12 21:31:30 +0200

    add permission for google map

--- lib/screens/home/map.dart
+++ lib/screens/home/map.dart
@@ -29,6 +29,7 @@ class _MapPageState extends State<MapPage> {
     rootBundle.loadString('assets/map_style.txt').then((string) {
       _mapStyle = string;
     });
+    checkPermission();
   }
 
   _onMapCreated(GoogleMapController controller) {
@@ -37,13 +38,54 @@ class _MapPageState extends State<MapPage> {
         controller.setMapStyle(_mapStyle);
       });
     super.initState();
+  }
 
+  checkPermission() async {
+    if (await Permission.location.request().isGranted &&
+        await Permission.locationWhenInUse.request().isGranted &&
+        await Permission.locationAlways.request().isGranted) {
+      return buildMap();
+    }
+    {
+      return askForLocation();
+    }
+  }
+
+  Widget buildMap() {
+    return GoogleMap(
+      markers: _markers,
+      trafficEnabled: false,
+      compassEnabled: true,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: true,
+      initialCameraPosition: CameraPosition(
+        target: LatLng(15.713290, 32.559315),
+        zoom: 10.2,
+      ),
+      onMapCreated: _onMapCreated,
+    );
+  }
+
+  Widget askForLocation() {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(
+            'Please enable location permission ...',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 50,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  GoogleMap(
+      body: GoogleMap(
         markers: _markers,
         trafficEnabled: false,
         compassEnabled: true,
