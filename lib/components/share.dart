6a7d394a88715a3d1aee914f33d2f514b8409cbb
oqbahdx <oqbahdx@gmail.com>
2022-02-22 14:30:37 +0200

    finish contact us page

---
+++ lib/components/share.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:hand_made_new/components/show_message.dart';
+
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:io';
+
+void launchFacebook(String url, String fallbackUrl) async {
+  try {
+    bool launched =
+    await launch(url, forceSafariVC: false, forceWebView: false);
+    if (!launched) {
+      await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+    }
+  } catch (e) {
+    await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+  }
+}
+
+launchWhatsapp(BuildContext context) async {
+  var whatsapp = "+249929990093";
+  var whatsappURlAndroid =
+      "whatsapp://send?phone=" + whatsapp + "&text=hello oqbah from dx player app";
+  var whatsappURLIos =
+      "https://wa.me/$whatsapp?text=${Uri.parse("hello oqbah form dx player app")}";
+  if (Platform.isIOS) {
+    // for iOS phone only
+    if (await canLaunch(whatsappURLIos)) {
+      await launch(whatsappURLIos, forceSafariVC: false);
+    } else {
+      showMessageError("Whatsapp is not installed");
+    }
+  } else {
+    // android , web
+    if (await canLaunch(whatsappURlAndroid)) {
+      await launch(whatsappURlAndroid);
+    } else {
+      showMessageError("Whatsapp is not installed");
+    }
+  }
+}
+
+Future<void> openEmail(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+void openUrl({String url}) {
+  launch(url, forceWebView: false, enableJavaScript: true);
+}
--- lib/screens/drawer/contact_us.dart
+++ lib/screens/drawer/contact_us.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hand_made_new/styles/colors.dart';
 import 'package:hand_made_new/widgets/app_bar.dart';
+import '../../components/share.dart';
 
 class ContactUs extends StatefulWidget {
   const ContactUs({Key key}) : super(key: key);
@@ -23,16 +25,80 @@ class _ContactUsState extends State<ContactUs> {
           action: Container()
         ),
         body: Container(
-          child: const Center(
-            child: Text('contact us',style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.bold
-            ),),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: gradientColor
+            )
           ),
-          decoration:
-              BoxDecoration(gradient: LinearGradient(colors: gradientColor)),
+          child: Center
+            (
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 150,
+                ),
+                GestureDetector(
+                  onTap: (){
+                    // launchEmail(context);
+
+                    openEmail("mailto:oqbahdx@gmail.com");
+                  },
+                  child: const Text(
+                    'oqbahdx@gmail.com',
+                    style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      color: Colors.black87,),
+                  ),
+                ),
+                const SizedBox(height: 250,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          launchFacebook('fb://profile/oqbah.dx.7',
+                              'https://www.facebook.com/oqbah.dx.7');
+                        },
+                        icon: const Icon(
+                          Icons.facebook,
+                          color: Colors.black87,
+                          size: 65,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          openUrl(url: 'https://github.com/oqbahdx');
+                        },
+                        icon:  const FaIcon(
+                          FontAwesomeIcons.github,
+                          color: Colors.black87,
+                          size: 60,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          openUrl(url: 'https://www.linkedin.com/in/oqbah-dx-25b9951b1/');
+                        },
+                        icon:  const FaIcon(
+                          FontAwesomeIcons.linkedin,
+                          color: Colors.black87,
+                          size: 60,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          launchWhatsapp(context);
+                        },
+                        icon:  const FaIcon(
+                          FontAwesomeIcons.whatsapp,
+                          color: Colors.black87,
+                          size: 60,
+                        )),
+                  ],)
+              ],
+            )),
         ),
-      ),
-    );
+          ),
+        );
+
+
   }
 }
